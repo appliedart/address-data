59552ecdce1e7d89c9e6d3849ff4c83480bc244d
Jason Northrup <digason@gmail.com>
2019-11-07 13:22:18 -0600

    Initial setup

---
+++ src/Plugin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace appliedart\addressdata;
+
+class Plugin extends \craft\base\Plugin
+{
+	public function init()
+	{
+		parent::init();
+
+		// Custom initialization code goes here...
+	}
+}
